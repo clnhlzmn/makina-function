6510b42f02ac8546f9ec983fc637443f56914ac7
Colin Holzman <colin.holzman@protonmail.com>
2021-03-19 09:08:49 -0400

    Initial commit.

---
+++ src/main/java/xyz/colinholzman/makina/MakinaFunction.java
@@ -0,0 +1,15 @@
+package xyz.colinholzman.makina;
+
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class MakinaFunction implements HttpFunction {
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws IOException {
+        BufferedWriter writer = response.getWriter();
+        writer.write("Hello World!");
+    }
+}
