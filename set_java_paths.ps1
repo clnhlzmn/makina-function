3826b8c9fcecad0e4ca6c550cad4274a38a90398
Colin <colin.holzman@protonmail.com>
2021-03-20 18:21:46 -0400

    Added set_java_paths.sh for mac and renamed build.ps1 to set_java_paths.ps1.

--- set_java_paths.ps1
+++ set_java_paths.ps1
@@ -1,4 +1,2 @@
 $env:JAVA_HOME = "C:\Program Files\AdoptOpenJDK\jdk-11.0.10.9-hotspot\"
 $env:Path = "C:\Program Files\AdoptOpenJDK\jdk-11.0.10.9-hotspot\bin\;" + $env:Path
-
-mvn compile
---
+++ set_java_paths.sh
@@ -0,0 +1,2 @@
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home"
+export PATH="/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/bin:$PATH"
