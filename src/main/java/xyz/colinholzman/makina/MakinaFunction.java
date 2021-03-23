75c84776392bc5947bf7255c00cc86483a333d6f
Colin Holzman <colin.holzman@protonmail.com>
2021-03-23 10:00:05 -0400

    Updated CodeGenerator ctor with additional args.

--- src/main/java/xyz/colinholzman/makina/MakinaFunction.java
+++ src/main/java/xyz/colinholzman/makina/MakinaFunction.java
@@ -14,6 +14,7 @@ import java.io.PrintWriter;
 import xyz.colinholzman.makina.Machine;
 import xyz.colinholzman.makina.Parse;
 import xyz.colinholzman.makina.CodeGenerator;
+import java.util.Collections;
 
 public class MakinaFunction implements HttpFunction {
     @Override
@@ -41,7 +42,7 @@ public class MakinaFunction implements HttpFunction {
         
         try {
             Machine machine = Parse.Companion.fileFromString(inputText);
-            CodeGenerator generator = new CodeGenerator(machine);
+            CodeGenerator generator = new CodeGenerator(machine, "void * ", "void * ", Collections.emptyList());
             
             generator.generateHeader(headerWriter);
             generator.generateSource(implementationWriter);
